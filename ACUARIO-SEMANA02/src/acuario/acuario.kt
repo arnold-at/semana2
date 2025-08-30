e76fbe3636d0292ceeecd21314e283aa64b0ea86
arnold-at <arnold.alva@tecsup.edu.pe>
2025-08-29 22:50:10 -0500

    Acuario: Herencia Terminado

--- ACUARIO-SEMANA02/src/acuario/acuario.kt
+++ ACUARIO-SEMANA02/src/acuario/acuario.kt
@@ -37,4 +37,23 @@ open class acuario(
     }
 }
 
+class TanqueTorre(
+    override var alto: Int,
+    var diametro: Int
+) : acuario(alto = alto, ancho = diametro, largo = diametro) {
+
+    override var volumen: Int
+        // área elíptica = π * r1 * r2
+        get() = (ancho / 2 * largo / 2 * alto / 1000 * PI).toInt()
+        set(valor) {
+            alto = ((valor * 1000 / PI) / (ancho / 2 * largo / 2)).toInt()
+        }
+
+    override var agua: Double
+        get() = volumen * 0.8
+        set(_) {}
+
+    override val forma = "cilindro"
+}
+
 
